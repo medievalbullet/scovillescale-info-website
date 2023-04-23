20529397c464e93aefb9cbc696b5151f2dd4fc05
medievalbullet <medievalbullet@freemail.hu>
2023-04-23 11:08:00 +0200

    made site more responsive on mobile devices

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,6 @@
 const header = document.querySelector("header")
 const main = document.querySelector(".main")
 
-// Set top margin according to the header size 
-let headerHeight = header.offsetHeight
-console.log("Header height: "+headerHeight)
-main.style.marginTop = String(headerHeight)+"px"
-
 //Scroll to to btn actions
 const scrolltotopBtn = document.querySelector("#scrolltotop-btn")
 
@@ -22,45 +17,53 @@ addEventListener("scroll", () => {
     }
 });
 
-// Section Dropdown
+// Section Dropdown Elements
 const sectionsDropdownGroup = document.querySelectorAll(".sections-dropdown-group")
-
 const sectionsDropdown = document.querySelector("#sections-dropdown")
-const dropdownBg = document.querySelector("#dropdown-bg")
-
-console.log("Sections group is: "+sectionsDropdownGroup)
-
 drowpdownGroupAction(sectionsDropdownGroup, sectionsDropdown)
 
-// Peppers Dropdown
+// Peppers Dropdown Elements
 const peppersDropdownGroup = document.querySelectorAll(".peppers-dropdown-group")
-
 const peppersDropdown = document.querySelector("#peppers-dropdown")
-
 drowpdownGroupAction(peppersDropdownGroup, peppersDropdown)
+
+// Others/General Elements
+const dropdownBg = document.querySelector("#dropdown-bg")
+const allDropdownGroup = document.querySelectorAll(".dropdown-groups")
 
 // Dropdown Action
 function drowpdownGroupAction(group, mainElement) {
     group.forEach(element => {
         
-        /*
-        element.addEventListener("mouseenter", () => {
-            mainElement.style.display = "flex"
-            dropdownBg.style.display = "unset"
-        })
-        element.addEventListener("mouseleave", () => {
-            mainElement.style.display = "none"
-            dropdownBg.style.display = "none"
-        })
-        */
         element.addEventListener("click", () => {
             if (mainElement.style.display == "flex") {
                 mainElement.style.display = "none"
                 dropdownBg.style.display = "none"
+                
+                // Hide all dropdown elements
+                allDropdownGroup.forEach(element => {
+                    element.style.display = "none"
+                })
+
             } else {
                 mainElement.style.display = "flex"
                 dropdownBg.style.display = "unset"
+                
+                // Hide not main elements
+                allDropdownGroup.forEach(element => {
+                    if (element != mainElement) {
+                        element.style.display = "none"
+                    }
+                })
             }
         })
     });
 }
+
+// Change main content top margin dynamically
+window.addEventListener('resize', () => {
+    let headerHeight = header.offsetHeight
+    console.log("Header height: "+headerHeight)
+    main.style.marginTop = String(headerHeight)+"px"
+});
+
